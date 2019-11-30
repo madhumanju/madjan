2e77e4172e1bd35dc20febbd360e6ce0993893f5
madhumanju <58061205+madhumanju@users.noreply.github.com>
2019-11-30 20:16:30 +0530

    completed

---
+++ addition.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+
+{
+    
+int a,b;
+    
+printf("enter two numbers");
+    
+scanf("%d%d",&a,&b);
+    
+while(b>0)
+    
+{
+        
+a++;
+        
+b--;
+    
+}
+    
+printf("%d",a);
+    
+return 0;
+
+
+}
---
+++ sumofdigits.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+ 
+int main() 
+{
+   
+int n;
+   
+int sum = 0;
+   
+printf("enter number");
+   
+scanf("%d",&n);
+ 
+   
+for (; n > 0; sum=sum + n % 10, n=n /10);
+ 
+   
+printf("Sumis %d", sum);
+return 0;
+
+}
