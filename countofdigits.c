69aa54431f38a81a971c2c9e16a68f446579b882
madhumanju <58061205+madhumanju@users.noreply.github.com>
2019-11-30 21:43:48 +0530

    completed

---
+++ countofdigits.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include<string.h>
+
+
+int main() 
+{
+   
+int num, dig;
+   
+char str[10];
+ 
+   
+printf("Enter number");
+   
+scanf("%d", &num);
+ 
+   
+sprintf(str, "%d", num);
+ 
+   
+dig = strlen(str);
+   
+printf("digits are %d", dig);
+ 
+   
+return(0);
+
+}
