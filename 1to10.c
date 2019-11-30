44d4ccf08dfd51827c1d80b1b04ccb36cd0dab38
madhumanju <58061205+madhumanju@users.noreply.github.com>
2019-11-30 19:28:27 +0530

    Add files via upload
    completed

---
+++ 1to10.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+  
+
+int main() 
+
+{ 
+    
+int i = 1; 
+    
+printf("%d ",i);
+
+begin: 
+   
+ i=i+1;
+    
+printf("%d ", i); 
+  
+    
+if (i < 10) 
+       
+ goto begin;
+ 
+    return 0;
+ 
+} 
