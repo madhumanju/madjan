73bc8a8527e34e5ae8f2423c407b3a78a423cdce
madhumanju <58061205+madhumanju@users.noreply.github.com>
2019-11-30 22:03:14 +0530

    completed

---
+++ swap.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main() 
+{
+    
+int a,b;
+   
+printf("enter two numbers");
+   
+scanf("%d%d",&a,&b);
+   
+printf("before swap %d%d",a,b);
+   
+b=a+b;
+   
+a=b-a;
+   
+b=b-a;
+   
+ 
+printf("after swap %d%d",a,b);
+   
+return(0);
+
+}
